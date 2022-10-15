cee8910f4327ce683e3db0e9f4070f7f8fbc7204
DESKTOP-AHTMLK3\sotni <sotnikov8383@gmail.com>
2022-10-15 13:47:58 +0300

    Update ClassWork.js

--- ClassWork.js
+++ ClassWork.js
@@ -1,7 +1,54 @@
-let data = prompt("Эй дай размер ребра куба?")
-function Vkuba(a){
-    let result = a*a*a;
-    return result;
-}
 
-alert("Объем равен " + Vkuba(data))
+let figura = prompt("Эй, человек) выбери фигуру которую будем мучать? \n 1. Куб\n 2. Паралелепипед\n 3. Прямоугольный параллелепипед\n 4. Призма\n 5. Пирамида\n (для валенков - введи нужную цифру)")
+switch(Number(figura)){
+            case 1:
+               let data = prompt("ТЫ ВЫБРАЛ КУБ\nТеперь выбери что будем считать?\n 1. Объем\n 2.Площадь\n 3.Диагонгаль")
+                switch (Number(data)) {
+                    case 1: let storona = prompt("Чему равна сторона куба?")
+                    if (Number(storona)) {
+                        alert(`Объем куба равен ${vKub(Number(storona))}.`);
+                    } else {
+                        alert("Валенок, введи число!!!")
+                    }
+                    break;
+                    case 2: 
+
+                    break;
+                    case 3:
+
+                    break;
+                    default: alert("Валенок, введи число от 1го до 3х!!!")
+                        
+                }
+             
+            break;
+            case 2:
+            alert("vibran paralel")
+            break;
+            case 3:
+
+            break;
+            case 4:
+
+            break;
+            case 5:
+
+            break;
+            default:  alert("Валенок, введи число от 1го до 5ти!!!")
+        }
+        function vKub(rebro) { // Объем куба
+            return Math.pow(rebro,3)
+        }
+        function sKub(params) { // Площадь куба
+            
+        }
+        function dKub(params) { // Диагональ куба
+            
+        }
+        function name(params) {
+            
+        }
+        
+
+
+
